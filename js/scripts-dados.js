1abded4e683bb976891b1071978ffb3edfdeb414
MdotSouza <micheljssouza@gmal.com>
2023-10-03 18:54:22 -0300

    Update dados-facilitados

--- js/scripts-dados.js
+++ js/scripts-dados.js
@@ -1,14 +1,213 @@
-const data = JSON.parse("js/dados.json");
-console.log(data);
+let dados = [
+    {
+        estado: "AC",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "AL",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "AM",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "AP",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "BA",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "CE",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "DF",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "ES",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "GO",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "MA",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "MG",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "MS",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "MT",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "PA",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "PB",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "PE",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "PI",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "PR",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "RJ",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "RN",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "RO",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "RR",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "RS",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "SC",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "SE",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "SP",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    },
+    {
+        estado: "TO",
+        acidentes: 0,
+        fatalidades: 0,
+        viaMaisPerigosa: "****",
+        principalCausa: "****"
+    }
+]
 
-let teste = `
-<div class="item-resumo" id="estado-01">
-    <div class="capa"></div>
-    <h3>Dom Casmurro</h3>
-    <h4>Machado de Assis</h4>
-    <p>Valor: R$11.66</p>
-    <button id="btn-subtrair-produto-01">-</button>
-    <input id="quantidade-produto-01" type="number" value="1" min="0">
-    <button id="btn-adicionar-produto-01">+</button>
-</div>
-`
+let sectionEstado = document.getElementById("resumo-estado")
+
+
+let cont = 1
+for (let dado of dados){
+    const divResumoEstado = document.createElement('div')
+    divResumoEstado.className = 'item-resumo'
+
+    divResumoEstado.id = `id-${cont}`
+    divResumoEstado.innerHTML = 
+    `
+        <h3>Estado: <span>${dado.estado}</span></h3>
+        <h4>Número de Acidentes: <span>${dado.acidentes}</span></h4>
+        <h4>Número de Fatalidades: <span>${dado.fatalidades}</span></h4>
+        <h4>Via Mais Perigosa: <span>${dado.viaMaisPerigosa}</span></h4>
+        <h4>Principal Causa de Acidente: <span>${dado.principalCausa}</span></h4>
+    `
+    sectionEstado.appendChild(divResumoEstado)
+    cont++
+}
+
