d7d70248ea7160ab7ccf7539b069701382917426
Vinicius_Rodrigues <viniciusrs1560@gmail.com>
2023-11-07 15:46:11 -0300

    atuv.6

--- asserts/js/rodape.js
+++ asserts/js/rodape.js
@@ -3,191 +3,131 @@
     const footerTemplate = document.createElement('template');
 
     footerTemplate.innerHTML =`
-
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Footer</title>
-    <link rel="stylesheet" href="./css/responsive_footer.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-
-    <!-- jQuery library -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
-
-    <!-- Popper JS -->
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-
-    <!-- font awesome -->
-    <script src="https://kit.fontawesome.com/1165876da6.js" crossorigin="anonymous"></script>
-    </head>
-
     <style>
-   
-    
-    footer{
-        width: 100%;
-        height: auto;
-        background: #282828;
-        padding-bottom: 20px;
-    }
-    
-    .square{
-        width: 100%;
-        height: auto;
-        margin-top: 48px;
-        padding: 0px !important;
-        padding-bottom: 20px;
-    }
-    
-    .brand{
-        margin-top: 20px;
-        font-size: 38px;
-        color: #ffffff;
-        font-weight: bold;
-    }
-    
-    .about{
-        width: 85%;
-        color: white;
-        margin-top: 5px;
-    }
-    
-    .heading{
-        margin-top: 20px;
-        font-size: 38px;
-        color: #ffffff;
-        font-weight: bold;
-        text-align: center;
-        
-        
-    }
- 
-    
-    .p-list{
-        padding: 0;
-        
-    }
-    
-    .p-list li{
-        display: block;
-        text-align: center;
-        margin: 5px 0px;
-        color: white;
-        
-    }
-    
-    .l-list{
-        padding: 0;
-        
-    }
-    
-    .l-list li{
-        display: block;
-        text-align: center;
-        margin: 5px 0px;
-        color: white;
-        
-    }
-    
-    .contact{
-        padding: 0;
-    }
-    
-    .contact li{
-        display: block;
-        text-align: center;
-        margin: 5px 0px;
-        color: white;
-    }
-    
-    .social-media{
-        text-align: center;
-        margin-top: 20px;
-    }
-    
-    .social-media li{
-        display: inline-flex;
-        color: white;
-        margin: 5px;
-        font-size: 25px;
-    }
-    
-    .copy{
-        text-align: center;
-        color: white;
-        margin-top: 20px;
-    }
-    
-    @media only screen and (max-width:767px){
-        .heading{
-            text-align: left;
+        *{
+            margin: 0;
+            padding: 0;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 16px;
+            color: white;
         }
-        .square{
-            padding: 0px 30px !important;
-            margin-top: 10px!important;
+        footer{
+            background: #343434;
+            padding-top: 50px;
         }
-    
-        .p-list li , .l-list li , .contact li{
-            text-align: left;
+        .container{
+            width: 1140px;
+            margin: auto;
+            display: flex;
+            justify-content: center;
         }
-        .social-media{
-            padding-left: 0!important;
+        .footer-content{
+            width: 33.3%;
         }
-    
-        .social-media li{
-            margin: 5px;
+        h3{
+            font-size: 28px;
+            margin-bottom: 15px;
+            text-align: center;
         }
-    } 
-
+        .footer-content p{
+            width:190px;
+            margin: auto;
+            padding: 7px;
+        }
+        .footer-content ul{
+            text-align: center;
+        }
+        .list{
+            padding: 0;
+        }
+        .list li{
+            width: auto;
+            text-align: center;
+            list-style-type:none;
+            padding: 7px;
+            position: relative;
+        }
+        .list li::before{
+            content: '';
+            position: absolute;
+            transform: translate(-50%,-50%);
+            left: 50%;
+            top: 100%;
+            width: 0;
+            height: 2px;
+            background: #f18930;
+            transition-duration: .5s;
+        }
+        .list li:hover::before{
+            width: 70px;
+        }
+        .social-icons{
+            text-align: center;
+            padding: 0;
+        }
+        .social-icons li{
+            display: inline-block;
+            text-align: center;
+            padding: 5px;
+        }
+        .social-icons i{
+            color: white;
+            font-size: 25px;
+        }
+        a{
+            text-decoration: none;
+        }
+        a:hover{
+            color: #f18930;
+        }
+        .social-icons i:hover{
+            color: #f18930;
+        }
+        .bottom-bar{
+            background: #f18930;
+            text-align: center;
+            padding: 10px 0;
+            margin-top: 50px;
+        }
+        .bottom-bar p{
+            color: #343434;
+            margin: 0;
+            font-size: 16px;
+            padding: 7px;
+        }
     </style>
-    <footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4 col-md-6 square">
-                <div class="brand">Empresa</div>
-                <p class="about">A BraVias é uma empresa dedicada a fornecer informações precisas e atualizadas sobre as rodovias do país.
-                 Nosso compromisso é facilitar a vida dos viajantes, motoristas e empresas que dependem das estradas brasileiras.
-                </p>
+        <footer>
+            <div class="container">
+                <div class="footer-content">
+                    <h3>Contatos</h3>
+                    <p>Email:Info@example.com</p>
+                    <p>Phone:+15 (11)) 565556</p>
+                    <p>Endereço:AV. Tomaz 235</p>
+                </div>
+                <div class="footer-content">
+                    <h3>Links Rápidos</h3>
+                     <ul class="list">
+                        <li class="nav-item"> <a href="index.html" class="nav-link">Home</a></li>
+                        <li class="nav-item"> <a href="sobre_nos.html" class="nav-link">Sobre</a></li>
+                        <li class="nav-item"> <a href="dados_facilitados.html" class="nav-link">Dados</a></li>
+                        <li class="nav-item"> <a href="cartilha.html" class="nav-link">Cartilhas</a></li>
+                        <li class="nav-item"> <a href="contato.html" class="nav-link">Contatos</a></li>
+                     </ul>
+                </div>
+                <div class="footer-content">
+                    <h3>Siga-nos</h3>
+                    <ul class="social-icons">
+                     <li><a href=""><i class="fab fa-facebook"></i></a></li>
+                     <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                     <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                     <li><a href=""><i class="fab fa-linkedin"></i></a></li>
+                    </ul>
+                    </div>
             </div>
-            <div class="col-lg-2 col-md-6 square">
-                <div class="heading">Informativo</div>
-                <ul class="p-list">
-                    <li>Product 1</li>
-                    <li>Product 2</li>
-                    <li>Product 3</li>
-                    <li>Product 4</li>
-                </ul>
+            <div class="bottom-bar">
+                <p>&copy; 2023 2023 sua empresa. Todos os direitos reservados</p>
             </div>
-            <div class="col-lg-2 col-md-6 square">
-                <div class="heading">Links</div>
-                <ul class="l-list">
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Sobre</a></li>
-                    <li><a href="">Dados</a></li>
-                    <li><a href="">Cartilhas</a></li>
-                    <li><a href="">Contatos</a></li>
-                </ul>
-            </div>
-            <div class="col-lg-4 col-md-6 square">
-                <div class="heading">Contatos</div>
-                <ul class="contact">
-                    <li><i class="fa-solid fa-house"></i> AV. Tomas de brito</li>
-                    <li><i class="fa-solid fa-envelope"></i> Email@gmail.com</li>
-                    <li><i class="fa-solid fa-envelope"></i> +4545 4544545</li>
-                </ul>
-            </div>
-        </div>
-        <ul class="social-media">
-            <li><i class="fa-brands fa-facebook"></i></li>
-            <li><i class="fa-brands fa-instagram"></i></li>
-            <li><i class="fa-brands fa-twitter"></i></li>
-        </ul>
-    </div>
-    <div class="copy">&copy Copyright & Reserved 2023. <a href="https://www.youtube.com">Tarun Code</a></div>
-</footer>
+        </footer>
           `;
 
           class Footer extends HTMLElement {
